11b1463a7a48784adeddadcb66b6e3e2cde9dace
Aliaksandr <kasasha@mail.ru>
2023-02-14 11:32:13 +0300

    Добавил метод getTriangleNumber и дописал main

--- HomeWork01/hw_001.java
+++ HomeWork01/hw_001.java
@@ -3,11 +3,13 @@ package HomeWork01;
 import java.util.Scanner;
 
 /**
- * hw_001
+ * hw_001 Вычисление n-го треугольного числа
  */
 public class hw_001 {
     public static void main(String[] args) {
-        System.out.println(getNumber("Введите число: "));
+        int number = getNumber("Введите число int: ");
+        long triangleNumber = getTriangleNumber(number);
+        System.out.printf("Треугольное число номер %d равно %d\n",number,triangleNumber);
     }
     
     static int getNumber(String request) {
@@ -16,5 +18,9 @@ public class hw_001 {
         int number = scanner.nextInt();
         scanner.close();
         return number;
+    }
+    static long getTriangleNumber(int number) {
+        long triangleNumber = number * (number + 1) / 2;
+        return triangleNumber;
     }
 }
