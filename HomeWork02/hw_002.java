cd537ee380673a752630ab52667418f158c7b6bc
Aliaksandr <kasasha@mail.ru>
2023-02-15 11:29:01 +0300

    добавил метод writeFile

--- HomeWork02/hw_002.java
+++ HomeWork02/hw_002.java
@@ -1,13 +1,22 @@
 package HomeWork02;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class hw_002 {
     public static void main(String[] args) throws Exception {
         Map<String, Integer> params = readParametersFromFile("HomeWork02/input.txt");
+        int a = params.get("a");
+        int b = params.get("b");
+        if (b == 0 & a == 0) {
+            writeFile("не определено");
+        }
+         
+
         params.forEach((key, value) -> System.out.println(key + ":" + value));
 
     }
@@ -19,9 +28,17 @@ public class hw_002 {
         String currentLine;
         while ((currentLine = bufferedReader.readLine()) != null) {
             String[] currentLineSplited = currentLine.split(" ");
-            map.put(currentLineSplited[0],Integer.parseInt(currentLineSplited[1]));
+            map.put(currentLineSplited[0], Integer.parseInt(currentLineSplited[1]));
         }
         bufferedReader.close();
         return map;
     }
+
+    private static void writeFile(String string) throws Exception {
+        File file = new File("output.txt");
+        PrintWriter pw = new PrintWriter(file);
+        pw.println(string);
+        pw.close();
+    }
+
 }
