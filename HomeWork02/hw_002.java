fd308fa678773948b255b6f823cf51c089e92c44
Aliaksandr <kasasha@mail.ru>
2023-02-15 13:04:38 +0300

    Добавил метод Pow

--- HomeWork02/hw_002.java
+++ HomeWork02/hw_002.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +17,9 @@ public class hw_002 {
         int b = params.get("b");
         if (b == 0 & a == 0) {
             writeFile("не определено");
+        } else {
+            writeFile(Pow(a, b));
         }
-         
-
-        params.forEach((key, value) -> System.out.println(key + ":" + value));
-
     }
 
     private static Map<String, Integer> readParametersFromFile(String pathName) throws Exception {
@@ -35,10 +36,23 @@ public class hw_002 {
     }
 
     private static void writeFile(String string) throws Exception {
-        File file = new File("output.txt");
+        File file = new File("HomeWork02/output.txt");
         PrintWriter pw = new PrintWriter(file);
         pw.println(string);
         pw.close();
+    }
+
+    private static String Pow(int a, int b) {
+        BigDecimal pow = BigDecimal.valueOf(1);
+        for (int i = 0; i < b; i++) {
+            pow = pow.multiply(BigDecimal.valueOf(a));
+        }
+        
+        for (int i = 0; i < (-b); i++) {
+            pow = pow.divide(BigDecimal.valueOf(a),100000, RoundingMode.HALF_UP );
+            //System.out.println(pow.stripTrailingZeros().toPlainString());
+        }  
+        return pow.stripTrailingZeros().toPlainString();
     }
 
 }
