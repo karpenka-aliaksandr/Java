068a3f12930e4d1077a3b84f23d4737237b2de88
Aliaksandr <kasasha@mail.ru>
2023-02-15 11:01:31 +0300

    Добавил метод readParametersFromFile

--- HomeWork02/hw_002.java
+++ HomeWork02/hw_002.java
@@ -1,22 +1,27 @@
 package HomeWork02;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class hw_002 {
     public static void main(String[] args) throws Exception {
-        Map<String, Integer> params = readParameters();
+        Map<String, Integer> params = readParametersFromFile("HomeWork02/input.txt");
         params.forEach((key, value) -> System.out.println(key + ":" + value));
 
     }
 
-    private static Map<String, Integer> readParameters() throws Exception {
-        int a = 0;
-        int b = 1;
-        Map<String, Integer> dict = new HashMap<>();
-        dict.put("b", b);
-        dict.put("a", a);
-        return dict;
+    private static Map<String, Integer> readParametersFromFile(String pathName) throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+        FileReader fileReader = new FileReader(pathName);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String currentLine;
+        while ((currentLine = bufferedReader.readLine()) != null) {
+            String[] currentLineSplited = currentLine.split(" ");
+            map.put(currentLineSplited[0],Integer.parseInt(currentLineSplited[1]));
+        }
+        bufferedReader.close();
+        return map;
     }
-
 }
